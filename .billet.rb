abdae9c0442e6d19e75385b64e57a24f4bccf5ae
Alexey Osipenko <alexey@osipenko.in.ua>
2021-10-24 19:27:44 +0300

    inline all_files

--- .billet.rb
+++ .billet.rb
@@ -1,6 +1,28 @@
 repo_root = 'https://raw.githubusercontent.com/cimon-io/billet-isolated-assets/master'
 assets_root_folder = 'assets'
+bin_folder = 'bin'
 initializers_folder = File.exist?('config/initializers/monkeypatches') ? 'config/initializers/monkeypatches' : 'config/initializers'
+
+inside assets_root_folder do
+  [
+    '.node-version',
+    'entrypoints.json',
+    'images/logo.png',
+    'webpack.config.js',
+    'yarn.lock',
+    'package.json',
+    'javascripts/landing.js',
+    'javascripts/application.js',
+    'javascripts/initializers/turbolinks.js',
+    'javascripts/initializers/ujs.js',
+    'stylesheets/application.scss',
+    'stylesheets/application.js',
+    'stylesheets/landing.scss',
+    'stylesheets/landing.js',
+  ].each do |filename|
+    get "#{repo_root}/#{filename}", filename
+  end
+end
 
 append_to_file '.gitignore' do <<~TEXT
 
@@ -12,12 +34,6 @@ append_to_file '.gitignore' do <<~TEXT
   /assets/yarn-debug.log*
   /assets/.yarn-integrity
 TEXT
-end
-
-inside assets_root_folder do
-  open("#{repo_root}/.allfiles").read.split("\n").each do |filename|
-    get "#{repo_root}/#{filename}", filename
-  end
 end
 
 inside bin_folder do
